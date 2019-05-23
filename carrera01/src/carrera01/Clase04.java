53aa2623f5f4bd82afc8adebaf577f0369696834
Emiliano N. Bessone <tito-02-01@hotmail.com>
2019-05-23 10:57:36 -0300

    se agrego cuarta clase

--- carrera01/src/carrera01/Clase04.java
+++ carrera01/src/carrera01/Clase04.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package carrera01;
 
 /**
  *
  * @author programador
  */
-public class clase2 {
-    
+public class Clase04 {
     
 }
